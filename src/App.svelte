38cb61933c80f5e804ca78233b3bfb51d262324a
Christopher Tian <mechristophertian@gmail.com>
2023-09-17 13:39:35 +0200

    added consumet api & movie component

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,87 @@
 <script>
-	export let name;
+  import MovieTile from "./components/MovieTile.svelte"
+  let name = 'You';
+  import { onMount } from 'svelte';
+  import axios from 'axios';
+
+  let movieList = [];
+  let movieData = null; // Define movieData at the component level
+  let searchValue = 'war';
+const handleInput = (e) => {
+  searchValue = e.target.value
+}
+  const searchData = async () => {
+    const url = `https://api.consumet.org/movies/dramacool/${searchValue}`;
+console.log(url)
+    try {
+      const response = await axios.get(url);
+      movieList = response.data.results;
+      console.log(movieList);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  const recommendData = async () => {
+    const url = "https://api.consumet.org/movies/dramacool/info?id=drama-detail/vincenzo";
+
+    try {
+      const { data } = await axios.get(url);
+      movieData = data; // Assign the response data to movieData
+      console.log(movieData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  onMount(() => {
+    recommendData();
+  });
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+  <h1>Hello {name}!</h1>
+  <input type="text" on:input={handleInput}>
+  <button type="button" on:click={searchData}>search</button>
+
+  <div>
+    {#if movieData}
+      <h5>Movie Title: {movieData.title}</h5>
+      <p>Description: {movieData.description}</p>
+      <!-- Display other movie information here -->
+    {:else}
+      <p>Loading...</p>
+    {/if}
+    {#each movieList as movie}
+    {#if movie}
+      <MovieTile movieTitle={movie.title} movieUrl={movie.url} movieImg={movie.image}/>
+    {:else}
+      <p>not found</p>
+    {/if}
+
+    {/each}
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+  text-align: center;
+  padding: 1em;
+  margin: 0 auto;
+}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+h1 {
+  color: #ff3e00;
+  text-transform: uppercase;
+  font-size: 4em;
+  font-weight: 100;
+}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+@media (min-width: 640px) {
+  main {
+    max-width: none;
+  }
+}
+
 </style>
---
+++ src/components/MovieTile.svelte
@@ -0,0 +1,32 @@
+<script>
+export let movieTitle;
+export let movieUrl;
+export let movieImg;
+
+</script>
+
+<tile>
+<div class="container">
+    <div class="img"><img src={movieImg} alt=""></div>
+    <a href={movieUrl}>
+        <div class="title">{movieTitle}</div>
+    </a>
+</div>
+</tile>
+
+<style>
+.container{
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    background-color: antiquewhite;
+    margin-bottom: 2rem;
+    padding: 2%;
+    border-radius: 2rem;
+}
+img{
+    max-width:200px; 
+    border-radius: 2rem;
+
+}
+</style>
